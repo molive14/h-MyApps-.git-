0ceb7a6eb9d782595e2cfcc056b4890d2ebdcf6b
Melvin Olive <melvinolive100@gmail.com>
2019-07-31 09:23:46 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,13 +4,7 @@ from flask import Flask, render_template, request, url_for, redirect
 app = Flask(__name__)
 app.debug = True 
 
-interviews = [{   "interviewee" : "Melvin Olive",
-                  "interviewer" : "Jai",
-                  "interviewerEmail": "Jai@gmail.com",
-                  "candidatesEmail" : "melvinolive100@gmail.com",
-                  "type" : "intern",
-                  "description" : "Developer at Chase",
-                  "time" : "7-25-2019" }]
+interviews = [{   }]
 
 
 
@@ -33,6 +27,22 @@ def interview():
         return redirect(url_for( "interview", interviews = interviews ))
     return render_template('interviews.html',interviews=interviews)
 
+@app.route('/candidates', methods=["POST", "GET"])
+def candidates():
+    if request.method == "POST": 
+       print(request.form["interviewee"])
+       interview =  {   "interviewee" : request.form["interviewee"],
+                  "interviewer" : request.form["interviewer"],
+                  "interviewerEmail": request.form["interviewee"],
+                  "candidatesEmail" : request.form["candidatesEmail"],
+                  "type" : request.form["type"],
+                  "description" : request.form["description"],
+                  "time" : request.form["time"]}
+       interviews.append(interview)
+       return redirect(url_for( "interview", interviews = interviews ))
+    return render_template('candidates.html',interviews=interviews)
+
+
 @app.route('/login')
 def index():
     return render_template('login.html')
@@ -45,13 +55,9 @@ def faq():
 def about():
     return render_template('about.html')
 
-@app.route('/candidates')
-def candidates():
-    return render_template('candidates.html')
 
 
 
 
 if __name__== "__main__":
     app.run()
-
