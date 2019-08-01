6e56a0fe553ba6efd4abdb60b33b3ebfdf40a80a
Melvin Olive <melvinolive100@gmail.com>
2019-08-01 12:00:16 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, url_for, redirect 
-
+import json
 
 app = Flask(__name__)
 app.debug = True 
 
 interviews = [{   }]
+
 
 
 
@@ -18,14 +19,28 @@ def interview():
         print(request.form["interviewee"])
         interview =  {   "interviewee" : request.form["interviewee"],
                   "interviewer" : request.form["interviewer"],
-                  "interviewerEmail": request.form["interviewee"],
+                  "interviewerEmail": request.form["interviewerEmail"],
                   "candidatesEmail" : request.form["candidatesEmail"],
-                  "type" : request.form["type"],
+                  "job" : request.form["job"],
                   "description" : request.form["description"],
                   "time" : request.form["time"]}
         interviews.append(interview)
-        return redirect(url_for( "interview", interviews = interviews ))
-    return render_template('interviews.html',interviews=interviews)
+     
+        # interview=json.dumps(interview)
+
+        print(interview)
+
+        print(type(interview))
+        with open("interviews.json", 'r') as f:
+            # f.append(interview)
+            current_interview_list = json.loads(f.read())
+        with open("interviews.json", 'w') as f:
+            current_interview_list[interview["time"]] = interview
+            print(current_interview_list)
+            json.dump(current_interview_list, f)
+            
+        return redirect(url_for( "interview", interviews = interviews,  ))
+    return render_template('interviews.html',interviews=interviews, )
 
 @app.route('/candidates', methods=["POST", "GET"])
 def candidates():
@@ -33,15 +48,24 @@ def candidates():
        print(request.form["interviewee"])
        interview =  {   "interviewee" : request.form["interviewee"],
                   "interviewer" : request.form["interviewer"],
-                  "interviewerEmail": request.form["interviewee"],
+                  "interviewerEmail": request.form["interviewerEmail"],
                   "candidatesEmail" : request.form["candidatesEmail"],
-                  "type" : request.form["type"],
+                  "job" : request.form["job"],
                   "description" : request.form["description"],
                   "time" : request.form["time"]}
        interviews.append(interview)
-       return redirect(url_for( "interview", interviews = interviews ))
-    return render_template('candidates.html',interviews=interviews)
 
+       return redirect(url_for( "interview", interviews = interviews ))
+    with open ("interviews.json", 'r') as f:
+       interviews2 = json.loads(f.read()) 
+       print (interviews2)
+       interviews3 = [ ]
+       for i in interviews2 :
+            interviews3.append(interviews2[i])
+
+        
+    return render_template('candidates.html',interviews=interviews3)
+    
 
 @app.route('/login')
 def index():
